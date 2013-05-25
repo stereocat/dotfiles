365fcaf057138a57f3d4908b18efd3db782a9fc0
stereocat <stereocat@gmail.com>
2013-05-25 18:09:35 +0900

    modified: setup.sh; add 'clean' command

--- setup.sh
+++ setup.sh
@@ -2,17 +2,32 @@
 
 DOT_DIR="$HOME/dotfiles"
 DOT_FILES=".screenrc .tmux.conf .emacs.el"
-for file in $DOT_FILES
-do
-	ln -s $DOT_DIR/$file $HOME/$file
-done
+case $1 in
+    install)
+	for file in $DOT_FILES
+	do
+	    ln -s $DOT_DIR/$file $HOME/$file
+	done
 
-if [ ! -e $HOME/.emacs.d ]
-then
-	mkdir $HOME/.emacs.d
-fi
+	if [ ! -e $HOME/.emacs.d ]
+	then
+	    mkdir $HOME/.emacs.d
+	fi
 
-for file in `ls $DOT_DIR/.emacs.d/`
-do
-	ln -s $DOT_DIR/.emacs.d/$file $HOME/.emacs.d/$file
-done
+	for file in `ls $DOT_DIR/.emacs.d/`
+	do
+	    ln -s $DOT_DIR/.emacs.d/$file $HOME/.emacs.d/$file
+	done
+	;;
+    clean)
+	for file in $DOT_FILES
+	do
+	    rm $HOME/$file
+	done
+	rm -rf $HOME/.emacs.d/
+	;;
+    *)
+	echo "usage: $0 [install|clean]"
+	;;
+esac
+
