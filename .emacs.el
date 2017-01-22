d7813d451520c74ef27b5e3ec0a9f768092a9faa
HAGIWARA Manabu <manabu.hagiwara@okinawaopenlabs.org>
2017-01-22 17:09:08 +0900

    add unfill-paragraph

--- .emacs.el
+++ .emacs.el
@@ -119,6 +119,19 @@
 (setq undo-strong-limit 90000)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; fill/unfill paragraph
+;; https://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; SKK (ddskk)
 
 (require 'skk-autoloads)
