33588481b3d606c1b27f9c1ce7e4b6a67c0dee6c
stereocat <stereocat@gmail.com>
2018-09-29 14:38:56 +0900

    update tmux configs

--- setup.sh
+++ setup.sh
@@ -4,30 +4,35 @@ DOT_DIR="$HOME/dotfiles"
 DOT_FILES=".screenrc .tmux.conf .emacs.el .latexmkrc"
 case $1 in
     install)
-	for file in $DOT_FILES
-	do
-	    ln -s $DOT_DIR/$file $HOME/$file
-	done
+        for file in $DOT_FILES
+        do
+            ln -s $DOT_DIR/$file $HOME/$file
+        done
 
-	if [ ! -e $HOME/.emacs.d ]
-	then
-	    mkdir $HOME/.emacs.d
-	fi
+        if [ ! -e $HOME/.emacs.d ]
+        then
+            mkdir $HOME/.emacs.d
+        fi
 
-	for file in `ls $DOT_DIR/.emacs.d/`
-	do
-	    ln -s $DOT_DIR/.emacs.d/$file $HOME/.emacs.d/$file
-	done
-	;;
+        # append alias/env files to .bashrc
+        BASHRC=~/.bashrc
+        sed -i -e "/^## begin dotfiles\/setup.sh/,/^## end dotfiles\/setup.sh/d" $BASHRC
+        cat .bashrc_append >> $BASHRC
+
+        for file in `ls $DOT_DIR/.emacs.d/`
+        do
+            ln -s $DOT_DIR/.emacs.d/$file $HOME/.emacs.d/$file
+        done
+        ;;
     clean)
-	for file in $DOT_FILES
-	do
-	    rm $HOME/$file
-	done
-	rm -rf $HOME/.emacs.d/
-	;;
+        for file in $DOT_FILES
+        do
+            rm $HOME/$file
+        done
+        rm -rf $HOME/.emacs.d/
+        ;;
     *)
-	echo "usage: $0 [install|clean]"
-	;;
+        echo "usage: $0 [install|clean]"
+        ;;
 esac
 
